f30e25e2647fb7fb0abe8fb192b5c759d8edba98
Robbie Hamill <robbierabhamill@gmail.com>
2016-10-18 16:24:43 +0100

    Minor testing changes

---
+++ cs547_readme.txt
@@ -0,0 +1,5 @@
+To run:
+Import the assignment into Eclipse
+
+The program will run the genetc algorithm. In the main class, to run the Hill Climber algorithm remove the /* and */ above and below the ///////////////////// lines and comment out the while loop and the System.out.println("Gen Number: "....) statement.
+To run the different data files, the 'faultNumber' and 'totalTests' variables in the main class will need to be changed depending on the file. The fileScanner() method will also need to be changed in the Population class, to change the name of the file.
--- src/Main.java
+++ src/Main.java
@@ -9,37 +9,71 @@ public class Main {
 		float crossoverRatio = 0.9f;// 0.2 // 0.1f = 10%
 		float mutationRatio = 0.15f; // 0.9// probability of mutation 
 		int faultNumber = 9; // 38; //9;
-		int geneLength = 5;
+		int geneLength = 100;
 		int totalTests = 216; // 1000;//216;
+		int noChanges = 0;
+		double previousFit = 0;
+		
 
 		for (int i = 0; i < 10; i++) {
 			long start = System.currentTimeMillis();
 			Population p = new Population();
 			p.createPopulation(populationSize, crossoverRatio, mutationRatio, faultNumber, geneLength, totalTests);
 			Chromosome bestGene = p.getPopulation()[0];
-			
-//			 HillClimber hc = new HillClimber(p);
-//			 for (int i = 0; i < 1000; i++) {
-//			 hc.compareFitness(hc.getStartingPoint(), totalTests);
-//			 System.out.println("Best solution with Hill Climber: " +
-//			 hc.bestSolution().getFitness());
-//			 }
 
-			while (bestGene.getFitness() != 1 && numberOfGenerations < 1000) {
-				p.evolve();
-				// p.random();
+			/*
+			///////////////////////////////////////////////////////////////////////////////////////////////////
+			 HillClimber hc = new HillClimber(p);
+			 for (int j = 0; j < 1000; j++) {
+				 double hcFit = hc.bestSolution().getFitness();
+			 	 hc.compareFitness(hc.getStartingPoint(), totalTests);
+			 	 if(hcFit == hc.bestSolution().getFitness()) {
+			 		 noChanges++;
+			 		 if(noChanges == 100) {
+			 			 System.out.println("Generation " + j);
+			 			 break;
+			 		 }
+			 	 }
+			 	 else {
+			 		 noChanges = 0;
+			 	 }
+			 }
+			 System.out.println("Best solution with Hill Climber: " + hc.bestSolution().getFitness());
+			 ///////////////////////////////////////////////////////////////////////////////////////////
+			  */
+
+			while (bestGene.getFitness() != 1 && numberOfGenerations < 1000 && noChanges != 100) {
+				//p.evolve();
+				p.random();
 				bestGene = p.getPopulation()[0];
-
-				// System.out.println(bestGene.getGene().toString());
-				// System.out.println("best " + bestGene.getFitness());
+				
+				if(bestGene.getFitness() == previousFit) {
+					noChanges++;
+				}
+				else {
+					noChanges = 0;
+				}
+				
+				//System.out.println("Generation " + numberOfGenerations + ", best fitness: " + bestGene.getFitness());
 				numberOfGenerations++;
+				previousFit = bestGene.getFitness();
 			}
+			
 			long finish = System.currentTimeMillis();
+			
+			/*
+			/////////////////////////////////////////////////////////////////////////////////////
+			System.out.println("Time elapsed in ms: " + (finish - start));
+			/////////////////////////////////////////////////////////////////////////////////////
+			*/
 
+			
 			System.out.println("Gen Number: " + numberOfGenerations + " BestGene: " + bestGene.getGene()
 				+ " Current Fitness " + bestGene.getFitness() + " Time elapsed in ms:" + (finish - start));
+			//System.out.println("\n");
 
 			numberOfGenerations = 1;
+			noChanges = 0;
 		}
 	}
 
